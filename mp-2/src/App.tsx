23794705a9d45487aacdb49199a877b543633b08
Vansh Diora <dioravansh@gmail.com>
2025-09-24 16:14:40 -0400

    changed without api key

--- mp-2/src/App.tsx
+++ mp-2/src/App.tsx
@@ -7,32 +7,18 @@ const ParentDiv = styled.div`
   margin: auto;
   border: 5px darkgoldenrod solid;
   padding: 2rem;
+  text-align: center;
 `;
 
 export default function App() {
   const [image, setImage] = useState<string>("");
-  const [breed, setBreed] = useState<string>("");
 
   useEffect(() => {
     async function fetchDogImage() {
       try {
-        const res = await fetch(
-          "https://api.thedogapi.com/v1/images/search?has_breeds=true&limit=1",
-          {
-            headers: {
-              "x-api-key": "live_OIW8swKt97Hi0yzCrReJgxonpWQVkNWhXtPgn6h9I1QBvEZh6EvUheSouHKNwJbT"
-            }
-          }
-        );
+        const res = await fetch("https://dog.ceo/api/breeds/image/random");
         const data = await res.json();
-        if (data.length > 0) {
-          setImage(data[0].url);
-          if (data[0].breeds && data[0].breeds.length > 0) {
-            setBreed(data[0].breeds[0].name);
-          } else {
-            setBreed("");
-          }
-        }
+        setImage(data.message); // 'message' contains the image URL
       } catch (error) {
         console.error("Error fetching dog image:", error);
       }
@@ -43,7 +29,7 @@ export default function App() {
   return (
     <ParentDiv>
       <h1>Random Dog Image</h1>
-      <DogDisplay imageUrl={image} breed={breed} />
+      <DogDisplay imageUrl={image} />
     </ParentDiv>
   );
 }
